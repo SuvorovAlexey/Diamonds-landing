beb705ccfce99a9027ab7eb47b864b2b28ab0820
Suvorov Aleksey <suvorov@hostco.ru>
2020-02-11 18:05:20 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+//кнопка бургер
+let menuContainer = document.querySelector('.menu-container')
+let menu = document.querySelector('.menu')
+let menuItem = document.querySelectorAll('.menu-item')
+
+let burger = document.querySelector('.burger')
+burger.addEventListener('click', ()=>{
+  menuContainer.classList.toggle('showContainer')
+  menu.classList.add('newNav')
+  for (var i = 0; i < menuItem.length; i++) {
+    menuItem[i].style.fontSize = '25px'
+    menuItem[i].style.marginBottom = '10px'
+  }
+})
+
+
+//кнопка-ссылка 'еще'
+let hiddenItem = document.querySelectorAll('.hidden')
+
+let elseLink = document.querySelector('.else-link')
+elseLink.addEventListener('click', (ev)=>{
+  ev.preventDefault()
+  for (var i = 0; i < hiddenItem.length; i++) {
+    hiddenItem[i].style.display = 'flex'
+  }
+elseLink.style.display = 'none'
+})
+
+
+//кнопка 'подобрать' и 'примерить'
+let btnOrder = document.querySelector('.pusets__info--btn')
+let btnTryOn = document.querySelectorAll('.goods-item-order--btn')
+let popupBG = document.querySelector('.popup-bg-deactive')
+let popupWindow = document.querySelector('.popup-window')
+let body = document.querySelector('body')
+
+btnOrder.addEventListener('click', ()=> {
+    popupBG.classList.add('popup-bg')
+    body.style.overflowY = 'hidden'
+  }
+)
+
+for (var i = 0; i < btnTryOn.length; i++) {
+  btnTryOn[i].addEventListener('click', ()=> {
+      popupBG.classList.add('popup-bg')
+      body.style.overflowY = 'hidden'
+    }
+  )
+}
+
+popupBG.addEventListener('click', (e)=> {
+  if (e.target.className == 'popup-window' || e.target.className == 'popup-window__input' || e.target.className == 'popup-window__label') {
+    return
+  }
+  popupBG.classList.remove('popup-bg')
+  body.style.overflowY = 'auto'
+  }
+)
